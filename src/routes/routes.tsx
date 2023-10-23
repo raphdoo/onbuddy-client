f3545759548d385d24d9cbcebc2786209183e32d
Minister-Isaac <isaacubakama@gmail.com>
2023-10-23 13:46:28 +0100

    auth pages

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import HomePage from '../pages/HomePage/Index'
+import { Login } from 'src/pages/login'
+import { Register } from 'src/pages/register'
+import { ConfirmPassword } from 'src/pages/confirm-password'
+import { ForgotPassword } from 'src/pages/forgot'
 
 function MainRoutes() {
   return (
@@ -11,7 +15,13 @@ function MainRoutes() {
 
         <Route path="/users" element={<h1>Users</h1>} />
 
-        <Route path="/contact" element={<h1>Contact</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/confirm-password" element={<ConfirmPassword />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+
+
         <Route
           path="*"
           element={
