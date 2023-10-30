0f9074ce2dfaee19118637c102091a0526c648f3
Skywonda <princesam3600@gmail.com>
2023-10-30 11:25:45 +0100

    mouted event routers

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,12 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import HomePage from '../pages/HomePage/Index'
-import AdminLayout from 'components/admin/HomePage/AdminLayout'
-import AdminHomePageLayout from '../components/admin/HomePageLayout/AdminHomePageLayout'
-import AdminWelcome from '../components/admin/HomePage/Purpose/AdminPurpose'
-import AdminPurpose from '../components/admin/HomePage/Purpose/AdminPurpose'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import HomePage from "../pages/HomePage/Index";
+import AdminLayout from "components/admin/HomePage/AdminLayout";
+import AdminHomePageLayout from "../components/admin/HomePageLayout/AdminHomePageLayout";
+import AdminWelcome from "../components/admin/HomePage/Purpose/AdminPurpose";
+import AdminPurpose from "../components/admin/HomePage/Purpose/AdminPurpose";
+import EventPage from "../pages/Event";
+import EventDetail from "components/Event/EventDetail";
+import Event from "components/Event";
 
 function MainRoutes() {
   return (
@@ -13,12 +16,21 @@ function MainRoutes() {
         <Route path="/about" element={<h1>About</h1>} />
         <Route path="/users" element={<h1>Users</h1>} />
         <Route path="/contact" element={<h1>Contact</h1>} />
+        <Route path="/event" element={<EventPage />} />
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<AdminHomePageLayout />} />
-          <Route
-            path="profile"
-            element={<h1 className="mt-[50px]">This is Profile</h1>}
-          />
+          <Route path="profile" element={<EventDetail />} />
+          <Route path="event" element={<EventPage />}>
+            <Route index={true} element={<Event />} />
+            <Route
+              path=":eventId"
+              element={
+                <h1 className="mt-[50px] bg-red-500">
+                  This is the event detail
+                </h1>
+              }
+            />
+          </Route>
           <Route path="home" element={<AdminHomePageLayout />}>
             <Route index element={<h1>This is profile</h1>} />
             <Route path="welcome" element={<h1>Welcome</h1>} />
@@ -31,13 +43,13 @@ function MainRoutes() {
           path="*"
           element={
             <div>
-              <h1>Page not Found 😢 </h1> <Link to={'/'}> Back to Home</Link>
+              <h1>Page not Found 😢 </h1> <Link to={"/"}> Back to Home</Link>
             </div>
           }
         />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default MainRoutes
+export default MainRoutes;
