15f14f2c69f643ca5da56a9ec7f6cc961444d599
Skywonda <princesam3600@gmail.com>
2023-11-11 14:15:20 +0100

    feat: improve navbar ui

--- src/components/admin/AdminNavBar/AdminNav.tsx
+++ src/components/admin/AdminNavBar/AdminNav.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { FiMenu } from 'react-icons/fi'
-import { logowhite, user } from '../../../assets/Assets'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
+import { logowhite, user } from "../../../assets/Assets";
 
 import {
   RiHome5Fill,
@@ -9,51 +9,53 @@ import {
   PiChatCenteredTextFill,
   PiNotepadFill,
   IoClose,
-} from '../../../assets/Icons'
-import { useDispatch, useSelector } from '../../../hooks/hooks'
-import { toggleProfile } from '../../../store/slices/ModalSlice'
+} from "../../../assets/Icons";
+import { useDispatch, useSelector } from "../../../hooks/hooks";
+import { toggleProfile } from "../../../store/slices/ModalSlice";
+import { links } from "../HomePageLayout/AdminHomePageLayout";
+import Collapsible from "components/common/CollapsMenu/Colllaps";
 
 const menuItems = [
   {
-    label: 'Home',
+    label: "Home",
     icon: <RiHome5Fill className="fill-white text-3xl" />,
-    link: '/home',
+    link: "/home",
   },
 
   {
-    label: 'Calendar',
+    label: "Calendar",
     icon: <IoCalendarNumber className="fill-white text-3xl" />,
-    link: '/calendar',
+    link: "/calendar",
   },
   {
-    label: 'Chat',
+    label: "Chat",
     icon: <PiChatCenteredTextFill className="fill-white text-3xl" />,
-    link: '/admin/home/socials',
+    link: "/admin/home/socials",
   },
   {
-    label: 'Tasks',
+    label: "Tasks",
     icon: <PiNotepadFill className="fill-white text-3xl" />,
-    link: '/tasks',
+    link: "/tasks",
   },
-]
+];
 
 const AdminNavbar: React.FC = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
-  const dispatch = useDispatch()
-  const Navigate = useNavigate()
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const dispatch = useDispatch();
+  const Navigate = useNavigate();
 
-  const profile = useSelector((state) => state.profileMenu.isOpen)
+  const profile = useSelector((state) => state.profileMenu.isOpen);
 
   const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen)
-  }
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   const toggleProfileDropdown = () => {
-    dispatch(toggleProfile())
-  }
+    dispatch(toggleProfile());
+  };
   const logout = () => {
-    Navigate('/signout')
-  }
+    Navigate("/signout");
+  };
 
   return (
     <nav className="bg-[#0F6FC8] md:px-8 sm:px-6  h-16 flex items-center justify-between fixed w-full ">
@@ -65,20 +67,39 @@ const AdminNavbar: React.FC = () => {
 
       <div className="flex items-center mr-4">
         {isMobileMenuOpen && (
-          <div className="sm:hidden fixed top-0 right-0 h-screen w-3/4 bg-blue-500 text-white p-4 transform-gpu translate-x-0 transition-transform">
-            <div className="relative flex justify-end">
+          <div className="sm:hidden fixed top-0 right-0 h-screen w-full bg-blue-50 text-white p-4 transform-gpu translate-x-0 transition-transform">
+            <div className="relative flex justify-end ">
               <IoClose
                 onClick={toggleMobileMenu}
-                className="right-0 text-2xl "
+                className="right-0 text-2xl"
+                color="black"
               />
             </div>
             <ul className="flex flex-col space-y-4">
-              {menuItems.map((item, index) => (
+              {/* {menuItems.map((item, index) => (
                 <li key={index}>
                   <Link to={item.link} className="text-white text-xl">
                     {item.label}
                   </Link>
                 </li>
+              ))} */}
+              {links.map((link) => (
+                <div key={link.title}>
+                  <Collapsible key={link.title} title={link.title}>
+                    {link.link.map((item) => (
+                      <Link
+                        to={`${item.path}`}
+                        key={item.label}
+                        className="hover:text-[#309CFF] text-xl text-[#454141] py-2 flex gap-2 font-normal"
+                        onClick={() => setIsMobileMenuOpen(false)}
+                      >
+                        <span className="ml-5">-</span>{" "}
+                        <li className="list-none">{item.label}</li>
+                      </Link>
+                    ))}
+                  </Collapsible>
+                  <div className="h-px bg-black "></div>
+                </div>
               ))}
             </ul>
           </div>
@@ -124,7 +145,7 @@ const AdminNavbar: React.FC = () => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default AdminNavbar
+export default AdminNavbar;
