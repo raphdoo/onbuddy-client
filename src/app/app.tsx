c714d13aee1f5ee4621b123bc3ee7a7443c78ebc
Rapheal Ajiboye <raphoooo95@gmail.com>
2023-11-20 01:39:42 +0100

    feat:contact search

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,11 @@
 import { useApi } from "hooks/api";
 import MainRoutes from "../routes/routes";
-import { useEffect } from "react";
 
 const App = () => {
   const [response] = useApi.get(`/auth/currentuser`);
   const { data: currentUser, isLoading } = response;
+
+  
 
 
   if (isLoading) return <div>Loading...</div>;
